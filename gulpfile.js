33acd8c551811878928c77b37075a6df9b32da57
ilyatoka <ilyatoka@yahoo.com>
2016-04-03 13:11:02 +0300

    Restructure project. Better error handling for gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    plumber     = require('gulp-plumber'),
     browserSync = require('browser-sync'),
     jade        = require('gulp-jade'),
     sass        = require('gulp-sass'),
@@ -11,7 +13,11 @@ var jekyll      = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll',
 // Build the Jekyll Site
 gulp.task('jekyll-build', function (done) {
   browserSync.notify(messages.jekyllBuild);
-  return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+  return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+    .on('error', function (error) {
+      gutil.log(gutil.colors.red(error.message));
+      this.emit('end');
+      })
     .on('close', done);
 });
 
@@ -32,11 +38,12 @@ gulp.task('browser-sync', ['sass', 'jade', 'jekyll-build'], function() {
 
 // Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
 gulp.task('sass', function () {
-  return gulp.src('assets/sass/main.scss')
-    .pipe(sass({
-      includePaths: ['sass'],
-      onError: browserSync.notify
+  return gulp.src('assets/sass/*.{scss,sass}')
+    .pipe(plumber(function (error) {
+        gutil.log(gutil.colors.red(error.message));
+        this.emit('end');
     }))
+    .pipe(sass({outputStyle: 'compact'}))
     .pipe(prefix(['last 2 versions'], { cascade: true }))
     .pipe(gulp.dest('_site/assets/css'))
     .pipe(browserSync.reload({stream:true}))
@@ -52,8 +59,8 @@ gulp.task('jade', function () {
 
 // Watch scss files for changes & recompile. Watch html/md files, run jekyll & reload BrowserSync
 gulp.task('watch', function () {
-  gulp.watch('_sass/*.scss', ['sass']);
-  gulp.watch('_jade/*.jade', ['jade']);
+  gulp.watch('assets/sass/**/*.{scss,sass}', ['sass']);
+  gulp.watch('_jade/**/*.jade', ['jade']);
   gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
